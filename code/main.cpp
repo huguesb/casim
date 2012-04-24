b4c19dafb3299d12ba3c1900586fa48f3dc5720b
Hugues Bruant <hugues.bruant@ensimag.fr>
2012-04-24 17:49:26 +0200

    Timing.

--- code/main.cpp
+++ code/main.cpp
@@ -11,6 +11,7 @@
 
 #include "pgm.h"
 #include "casim.h"
+#include "cycleTimer.h"
 
 #include <cstdio>
 #include <cstdlib>
@@ -22,15 +23,37 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
     
+    double ref = CycleTimer::currentSeconds();
+    
     PGM input;
     CASim sim(argv[3]);
     
-    if (!input.load(argv[1])) { return EXIT_FAILURE; }
+    if (!input.loadPattern(argv[1])) { return EXIT_FAILURE; }
+    
+    double ldr = CycleTimer::currentSeconds();
+    
     sim.setCells(input.width(), input.height(), input.white(), input.data());
+    
+    double set = CycleTimer::currentSeconds();
     
     sim.step(atoi(argv[4]));
     
+    double stp = CycleTimer::currentSeconds();
+    
     sim.getCells(input.data());
+    
+    double get = CycleTimer::currentSeconds();
+    
     if (!input.save(argv[2])) { return EXIT_FAILURE; }
+    
+    double str = CycleTimer::currentSeconds();
+    
+    fprintf(stderr, "Load  : %lf ms\n", (ldr - ref)*1000.0);
+    fprintf(stderr, "Set   : %lf ms\n", (set - ldr)*1000.0);
+    fprintf(stderr, "Sim   : %lf ms\n", (stp - set)*1000.0);
+    fprintf(stderr, "Get   : %lf ms\n", (get - stp)*1000.0);
+    fprintf(stderr, "Store : %lf ms\n", (str - get)*1000.0);
+    fprintf(stderr, "Total : %lf ms\n", (str - ref)*1000.0);
+    
     return EXIT_SUCCESS;
 }
